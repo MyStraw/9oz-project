4ac2783be3b167e18e9a3c65e534823f290f1ac0
Icecaptin <tjdqn0228@gmail.com>
2023-09-15 17:37:54 +0900

    20230915_"최종" JWT 로그인구현, 크롤링동작버튼추가, 로그인기능, 각종 수정 및 보완

--- src/components/CrawlButton.js
+++ src/components/CrawlButton.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from '@mui/material/Button';
 import axios from 'axios';
 
 const CrawlButton = () => {
     const [isLoading, setIsLoading] = useState(false);
+    const [isCompleted, setIsCompleted] = useState(false);
+    const [crawlStatus, setCrawlStatus] = useState('');
 
     const handleCrawl = () => {
         setIsLoading(true);
+        setIsCompleted(false);
+        setCrawlStatus('');
 
-        // localStorage에서 토큰 가져오기
         const token = localStorage.getItem('accesstoken');
 
         const config = {
@@ -18,15 +21,31 @@ const CrawlButton = () => {
         };
 
         axios.post("http://10.125.121.170:8080/crawl", null, config)
-            .then(() => {
+            .then(response => {
                 setIsLoading(false);
+                setIsCompleted(true);
                 console.log('Crawl 버튼을 눌렀습니다.');
+                if (response.data.status) {
+                    setCrawlStatus(response.data.status);
+                }
             })
             .catch(error => {
                 setIsLoading(false);
+                setIsCompleted(false);
                 console.error('Crawl 중 오류 발생:', error);
             });
     };
+
+    useEffect(() => {
+        if (isCompleted) {
+            const timer = setTimeout(() => {
+                setIsCompleted(false);
+                setCrawlStatus('');
+            }, 5000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [isCompleted]);
 
     return (
         <>
@@ -34,6 +53,8 @@ const CrawlButton = () => {
                 Crawl
             </Button>
             {isLoading && <p>크롤링 중입니다...</p>}
+            {isCompleted && <p>{setCrawlStatus}</p>}
+            {crawlStatus && <p>{crawlStatus}</p>}
         </>
     )
 }
